d78720b7138dae003b5cd9c6b2315671ea824c23
vinacovre <viniassis@me.com>
2017-05-05 15:34:07 -0300

    fixing error with lea

--- 3rd Class/mov_inst8.asm
+++ 3rd Class/mov_inst8.asm
@@ -12,9 +12,9 @@ global _start
 _start:
 	
 	mov ebx,[char]				; Content (value of 'A' in ASCII table)
-	;mov ebx,char				; Address
-	;lea ebx,[char]				; Address
-	;lea ebx, char				; Address
+	; mov ebx,char					; Address
+	; lea ebx,[char]				; Address
+	; lea ebx, char					; ERROR: invalid compbination
 
 	mov eax,1 					; exit command to kernel
 	int 0x80 					; interrupt 80 hex, call kernel
