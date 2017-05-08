a3bdf3fe0f671cf430bd36f9ba10610ebc17254c
vinacovre <viniassis@me.com>
2017-05-08 01:08:14 -0300

    explaining code

--- previous years tests/P1/2b.asm
+++ previous years tests/P1/2b.asm
@@ -10,13 +10,13 @@ global _start
 
 _start:
 
-    mov     ecx, var1
-    inc     ecx
-    mov     edx, var2
-    inc     edx
-    xchg    ecx, edx
-    mov     ebx, [ecx]
-    sub     ebx, [edx]
+    mov     ecx, var1   ; ecx points to the address of var1 (&var1[0])
+    inc     ecx         ; increment ecx... now points to the next position (which ir &var2[0])
+    mov     edx, var2   ; edx points to the address of var2 (&var2[0])
+    inc     edx         ; increment edx... now points to the next position (which ir var3[0])
+    xchg    ecx, edx    ; ecx <--> edx ... ecx points to &var3[0] and edx points to &var2[0]
+    mov     ebx, [ecx]  ; ebx gets the content in &var3[0]... ebx = 7
+    sub     ebx, [edx]  ; ebx = ebx - (content in &var2[0])... ebx = 7 - 3 = 4
 
     mov     eax, 1
     int     0x80
