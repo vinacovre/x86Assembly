981d80242c38e493d6ed98ac09fb6558b521976c
ritterjonas <jonas_ritter@yahoo.com.br>
2017-05-29 22:08:44 -0300

    Add files via upload

--- 7th Class/proc4.c
+++ 7th Class/proc4.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
 
-// https://www.ibiblio.org/gferg/ldp/GCC-Inline-Assembly-HOWTO.html
-// Compile: gcc -m32 proc4.c
-// Install: sudo apt-get install gcc-multilib
-
-int sum_asm (int n1, int n2);
-
 int main () {
-    int v1=10;
-    int v2=30;
-    int r = sum_asm(v1,v2);
-    printf("Sum value: %d\n", r);
+	int v1=10; int v2=30;
+	int r = sum_asm(v1,v2);
+	printf("Valor da Soma: %d \n", r);
 }
 
-int sum (int n1, int n2) {
-    return n1+n2;
+int sum (int n1, int n2){
+	return n1+n2;
 }
 
 int sum_asm (int n1, int n2) {
-    asm ("mov 12(%ebp), %ebx");
-    asm ("mov 8(%ebp), %eax");
-    asm ("add %ebx, %eax");
-    // asm ("mov 12(%ebp), %eax");
+	asm("mov 12(%ebp), %ebx");
+	asm("mov 8(%ebp), %eax");
+	asm("add %ebx, %eax");
+	//asm("mov 12(%ebp), %eax")
 }
