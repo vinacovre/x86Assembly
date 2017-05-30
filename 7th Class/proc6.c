c8ca7c4b36a0babda451541f452e3be543dcbccb
ritterjonas <jonas_ritter@yahoo.com.br>
2017-05-29 22:18:13 -0300

    Uploading external function example

---
+++ 7th Class/proc6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// as --32 -o sum.o sum.s
+// gcc -m32 -o proc6.out proc6.c sum.o
+
+int main () {
+  int v1=10;
+  int v2=30;
+  int r=0;
+  extern int sum_asm (int, int);
+  r = sum_asm(v1,v2);
+  printf("Valor da soma: %d \n", r);
+}
---
+++ 7th Class/sum.s
@@ -0,0 +1,10 @@
+  .text
+  .global sum_asm
+
+sum_asm:
+  push %ebp
+  mov %esp,%ebp
+  mov 8(%ebp), %eax
+  add 12(%ebp), %eax
+  pop %ebp
+  ret
